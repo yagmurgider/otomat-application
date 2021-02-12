7552d2d2a96d4bec9b8f9fa8038a4b679d29bfc5
Yağmur Gider <ygmrgider@gmail.com>
2021-02-13 02:29:58 +0300

    Initial commit

---
+++ src/main/java/com/odeal/otomat/OtomatApplication.java
@@ -0,0 +1,15 @@
+package com.odeal.otomat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtomatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtomatApplication.class, args);
+    }
+
+}
+
+
---
+++ src/test/java/com/odeal/otomat/OtomatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odeal.otomat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtomatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
